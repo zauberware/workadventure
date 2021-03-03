f312b4e0543859d3b8e5594d35366560d2eac220
simonfranzen <simon@zauberware.com>
2021-03-03 19:32:27 +0000

    Deploying to gh-pages from master @ 418831975d623fee95ce4a627a75111fcb32db9b 🚀

---
+++ Resource-ExampleEfficientTileset.tsx
@@ -0,0 +1,44 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.3" name="Resource-ExampleEfficientTileset" tilewidth="32" tileheight="32" tilecount="345" columns="15">
+ <image source="../../Downloads/Resource-ExampleEfficientTileset.png" width="480" height="736"/>
+ <tile id="45">
+  <properties>
+   <property name="openWebsite" value="https://www.gusto.at/rezepte/zufall"/>
+  </properties>
+ </tile>
+ <tile id="46">
+  <properties>
+   <property name="openWebsite" value="https://www.gusto.at/rezepte/zufall"/>
+  </properties>
+ </tile>
+ <tile id="47">
+  <properties>
+   <property name="collides" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="48">
+  <properties>
+   <property name="collides" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="60">
+  <properties>
+   <property name="openWebsite" value="https://www.gusto.at/rezepte/zufall"/>
+  </properties>
+ </tile>
+ <tile id="61">
+  <properties>
+   <property name="openWebsite" value="https://www.gusto.at/rezepte/zufall"/>
+  </properties>
+ </tile>
+ <tile id="62">
+  <properties>
+   <property name="collides" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="63">
+  <properties>
+   <property name="collides" type="bool" value="true"/>
+  </properties>
+ </tile>
+</tileset>
